e9c887555685f4dbecd10179165a79b39bd690fa
mansiey <mansi46rd@gmail.com>
2026-05-16 14:48:52 +0530

    write winning logic

--- 01-tic tac toe/script.js
+++ 01-tic tac toe/script.js
@@ -2,6 +2,8 @@ console.log("welcome totic-tac-toe");
 
 let tapAudio = new Audio("tap_sound.mp3");
 let nextTurnAudio = new Audio("next_turn.mp3");
+let winningAudio = new Audio("won_sound.mp3");
+let isGameOver = false;
 
 let turn = "X";
 
@@ -11,13 +13,36 @@ const changeTurn = () => {
 }
 
 //function if won
+let winningPattern = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
+];
+
 const checkWin = () => {
+    let boxTexts = document.querySelectorAll('.boxtext');
+
+
+    winningPattern.forEach((pattern) => {
+
+        let [a, b, c] = pattern;
+
+        if (boxTexts[a].innerText !== '' && (boxTexts[a].innerText === boxTexts[b].innerText) && (boxTexts[a].innerText === boxTexts[c].innerText)) {
+            isGameOver = true;
+            document.querySelector('.info').innerText = boxTexts[a].innerText + " WON!! "
+            document.querySelector('.imgBox').getElementsByTagName('img')[0].style.width = "150px";
+            winningAudio.play();
+        }
+    })
+    
 
 }
 
 //Playing part
-let boxes = document.getElementsByClassName('box');
-Array.from(boxes).forEach((ele) => {
+// let boxes = document.getElementsByClassName('box');
+let boxes = document.querySelectorAll('.box');
+boxes.forEach((ele) => {
+    // Array.from(boxes).forEach((ele) => {
     let boxText = ele.querySelector('.boxtext');
     ele.addEventListener('click', function () {
         tapAudio.play();
@@ -26,7 +51,10 @@ Array.from(boxes).forEach((ele) => {
             turn = changeTurn();
             nextTurnAudio.play();
             checkWin();
-            document.getElementsByClassName("info")[0].innerText = "Turn For " + turn;
+            if (!isGameOver) {
+                document.getElementsByClassName("info")[0].innerText = "Turn For " + turn;
+            }
+            
         }
     })
 })
